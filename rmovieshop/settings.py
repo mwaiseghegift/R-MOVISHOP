7e5e17aced4c6fdf24b25044192f163ca8a13772
Gateway47 <itsregalo047@gmail.com>
2021-05-03 14:11:58 +0300

    updated homepage

--- rmovieshop/settings.py
+++ rmovieshop/settings.py
@@ -121,6 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIR = [
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'frontend/static')
 ]
