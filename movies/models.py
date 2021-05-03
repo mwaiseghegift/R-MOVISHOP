633ee19de809dd933cc0f8f5cacbee53061813a1
Gateway47 <itsregalo047@gmail.com>
2021-05-03 23:19:56 +0300

    created base models

--- movies/models.py
+++ movies/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 from django.utils.text import slugify
 # Create your models here.
-
-
+PG = [
+    ('General','4+'),
+    ('PG','13+'),
+    ('Teen','16+'),
+    ('Adult','18+')
+]
 class Category(models.Model):
     category_name = models.CharField(max_length=255)
     slug = models.SlugField(blank=True)
@@ -19,7 +23,7 @@ class Tag(models.Model):
      
      def get_tag_movies(self):
          return Movie.objects.filter(tags__icontains=self)
-        
+     
 class Movie(models.Model):
     title = models.CharField(max_length=255)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
@@ -27,4 +31,20 @@ class Movie(models.Model):
     cover_image = models.ImageField(upload_to="images/movies/covers")
     tags = models.ManyToManyField(Tag)
     slug = models.SlugField(blank=True)
+    parental_guidance = models.CharField(choices=PG, max_length=50)
+    date_released = models.DateTimeField()
+    
+class Trailer(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    link = models.CharField(max_length=255)
+    
+class Rating(models.Model):
+    user = models.ForeignKey(User, on_ddelete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    
+class MovieReview(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TectField()
+    date_added = models.DateTimeField(auto_now_add=True)
+    is_approved = models.BooleanField(default=False)
     
