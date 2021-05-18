39e1426878f03ade2826e8e568214e3ffd6469a2
Gateway47 <itsregalo047@gmail.com>
2021-05-18 23:19:10 +0300

    updating views

--- movies/views.py
+++ movies/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Movie, TvSeries
 # Create your views here.
 
@@ -13,6 +13,24 @@ def IndexView(request, *args, **kwargs):
     } 
     return render(request, 'index.html', {})
 
+
+def MovieDetailView(request, slug,*args, **kwargs):
+    movie = get_object_or_404(Movie, slug=slug)
+    
+    context = {
+        'movie':movie,
+    }
+    return render(request, 'movie_detail.html', context)
+
+
+def TvSerieDetailView(request, slug, *args, **kwargs):
+    serie = get_object_or_404(TvSeries, slug=slug)
+
+    context = {
+        'series':serie,
+    }
+    return render(request, 'serie_detail.html', context)
+    
 def catalog(request, *args, **kwargs):
     return render(request, 'catalog.html')
 
