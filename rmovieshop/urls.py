54f5b52ba40165f4b72e37d61a5698e9caf34866
Gateway47 <itsregalo047@gmail.com>
2021-05-21 23:52:03 +0300

    urls

--- rmovieshop/urls.py
+++ rmovieshop/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.conf.urls import handler404
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('movies.urls', namespace='movies')),
     path('user/', include('User.urls', namespace='User')),
-]
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
